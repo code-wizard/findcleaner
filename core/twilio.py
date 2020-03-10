1d8df16104390e08fce83d84cad3da6c52877959
Ebuka <amaefulacj@gmail.com>
2020-03-10 07:06:01 -0230

    Twilio sms integration

---
+++ core/twilio.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from twilio.rest import Client
+
+client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+
+
+class TwilioSMS(object):
+
+    def send_otp(self, to, channel="sms"):
+        if channel not in ("sms", "voice"):
+            channel = "sms"
+
+        verification = client.verify \
+            .services(settings.VERIFICATION_SID) \
+            .verifications \
+            .create(to=to, channel=channel)
+
+        return verification.sid
+
+    def check_verification(self, phone, code):
+        service = settings.VERIFICATION_SID
+
+        try:
+            verification_check = client.verify \
+                .services(service) \
+                .verification_checks \
+                .create(to=phone, code=code)
+
+            if verification_check.status == "approved":
+
+                return True, "Your phone number has been verified! Please login to continue."
+
+            else:
+                return False, "The code you provided is incorrect. Please try again."
+        except Exception as e:
+            return False, "Error validating code: {}".format(e)
