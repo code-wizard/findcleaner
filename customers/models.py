0ac6436176eb9204b38bf3c6384f7e8d77cae6b8
hasmeed <hasmeedcoder@gmail.com>
2020-08-16 22:32:24 -0400

    change email length on help model

--- customers/models.py
+++ customers/models.py
@@ -101,7 +101,7 @@ class FcServiceRequest(models.Model):
 
 class FcHelp(models.Model):
     phone = models.CharField(max_length=11)
-    email = models.EmailField(max_length=20)
+    email = models.EmailField(max_length=40)
 
     class Meta:
         verbose_name = "Help"
