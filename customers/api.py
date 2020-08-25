5f83527c701a726b687d3b9a761fb01b479cc3e3
hasmeed <hasmeedcoder@gmail.com>
2020-08-25 06:27:50 -0400

    fix provider distance

--- customers/api.py
+++ customers/api.py
@@ -130,7 +130,8 @@ class FcSearchProviders(generics.ListAPIView):
         neighborhood = self.request.GET.get("neighborhood","")
         locality = self.request.GET.get("locality","")
         # qs = qs.filter(Q(provider__address__icontains=neighborhood) & Q(provider__state__icontains=locality) | Q(provider__city__icontains=locality))
-        qs = qs.filter( Q(provider__state__icontains=locality) | Q(provider__city__icontains=locality))
+        # qs = qs.filter( Q(provider__state__icontains=locality) | Q(provider__city__icontains=locality))
+        qs = qs.order_by('distance')
         return qs
 
     def get_serializer_context(self):
