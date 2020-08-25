97acf21f83447dfe04c3c938b30f529f1d3f58cb
hasmeed <hasmeedcoder@gmail.com>
2020-08-25 06:30:46 -0400

    fix provider distance

--- customers/api.py
+++ customers/api.py
@@ -130,7 +130,7 @@ class FcSearchProviders(generics.ListAPIView):
         neighborhood = self.request.GET.get("neighborhood","")
         locality = self.request.GET.get("locality","")
         # qs = qs.filter(Q(provider__address__icontains=neighborhood) & Q(provider__state__icontains=locality) | Q(provider__city__icontains=locality))
-        # qs = qs.filter( Q(provider__state__icontains=locality) | Q(provider__city__icontains=locality))
+        qs = qs.filter( Q(provider__state__icontains=locality) | Q(provider__city__icontains=locality))
         qs = qs.order_by('distance')
         return qs
 
